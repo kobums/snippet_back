8109083f9bea6ce7957af8d6ae91cb8b44a243e3
고범석 <kobums@naver.com>
2026-03-11 20:03:41 +0900

    refactor(dto): 불필요한 단일 필드 DTO 삭제 및 기존 DTO 코멘트 수정

--- src/main/java/com/snippet/dto/LibraryAddRequestDto.java
+++ src/main/java/com/snippet/dto/LibraryAddRequestDto.java
@@ -13,8 +13,8 @@ public class LibraryAddRequestDto {
     private String isbn;
     private String coverUrl;
     private Integer totalPage;
-    private String type; // wish, borrow, have
-    private String status; // waiting, reading, completed, dropped
+    private String type; // wish, have, borrow, return
+    private String status; // none(wish 전용), waiting, reading, completed, dropped
     private String startDate; // yyyy-MM-dd or yyyy-MM-ddTHH:mm:ss
     private String endDate; // yyyy-MM-dd or yyyy-MM-ddTHH:mm:ss
 }
--- src/main/java/com/snippet/dto/RecordAddRequestDto.java
+++ src/main/java/com/snippet/dto/RecordAddRequestDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class RecordAddRequestDto {
+    private Long bookId;
     private String type; // "snippet", "diary", "review"
     private String text;
     private String tag;
